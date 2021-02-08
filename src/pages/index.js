c52fd8365a031e32777e377de78d721197404e97
Bart Dubois <dubcio@o2.pl>
2021-02-08 22:32:01 +0100

    Add the origami.network logo (#9)
    Changed:
    * Logo and quote on main page.
    * Link to example 404 page (to not create errors when preloading pages by links).
    
    Added:
    * Logo image recreated step by step from previous designs.
    * Image plugins for Gatsby.
    
    Issue: #3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-export default () => {
+export default ({ data }) => {
   return (
     <main>
       <title>origami.network</title>
@@ -15,8 +16,22 @@ export default () => {
       <h2>Second success</h2>
       <p>
         The page is generated using <a href="https://www.gatsbyjs.com/">gatsby.js</a>!.
-        It has also <Link to="/something-that-do-not-exists">404 status page</Link>.
+        It has also <Link to="/404">404 status page</Link>.
       </p>
+
+      <h2>Third success</h2>
+      <p>The page has logo and quote.</p>
+      <img src={data.logo.src} alt="origami.network logo" />
+      <blockquote>
+        origami.network - the art of software folding
+      </blockquote>
     </main>
   )
 }
+
+export const query = graphql`
+  query {
+    logo: file(relativePath: { eq: "logo.svg" }) {
+      src: publicURL
+    }
+  }`
