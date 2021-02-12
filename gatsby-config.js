ed8a40c9a606afcb109d8c5c4f2061335d7d61c8
Bart Dubois <dubcio@o2.pl>
2021-02-12 01:05:51 +0100

    Add blog content and transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,21 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-      },
+      }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/content/blog`,
+      }
+    },    
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
-  ],
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-asciidoc`,
+      options: {
+      }
+    }
+  ]
 };
