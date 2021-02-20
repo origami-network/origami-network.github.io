670cc93dee7c2681f476e40fee97eccb270bee7a
Bart Dubois <dubcio@o2.pl>
2021-02-21 00:49:15 +0100

    Add logo on 404 status page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-export default () => {
+export default ({data}) => {
   return (
     <main>
       <title>404 - page not found</title>
+      <img src={data.logo.src} alt="origami.network logo" />
       <h1>Page not found</h1>
       <p>
         Sorry, the page is not know.
@@ -13,3 +15,11 @@ export default () => {
     </main>
   )
 }
+
+export const query = graphql`
+  query {
+    logo: file(relativePath: { eq: "logo.svg" }) {
+      src: publicURL
+    }
+  }
+`;
